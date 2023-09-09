3715b24c9dae26fe85305427a1e88aa332e2e8fb
bieu bobo <gabrielmmbonfim@gmail.com>
2023-09-09 20:30:25 -0300

    arquivo de raça desbugado

---
+++ models/Race.js
@@ -0,0 +1,62 @@
+const { default: mongoose } = require("mongoose");
+
+const raceSchema =  mongoose.Schema(
+    {
+        name: {
+          type: String
+        },
+        source: {
+          type: String
+        },
+        page: {
+          type: Number
+        },
+        size: {
+          type: String
+        },
+        speed: {
+          type: Number
+        },
+        ability: {
+            str: {
+                type: Number,
+                required: false
+              },
+              dex: {
+                type: Number,
+                required: false
+              },
+              con: {
+                type: Number,
+                required: false
+              },
+              int: {
+                type: Number,
+                required: false
+              },
+              wis: {
+                type: Number,
+                required: false
+              },
+              cha: {
+                type: Number,
+                required: false
+              }
+        },
+        darkvision: {
+          type: Number
+        },
+        entries: {
+          type: [
+            Array
+          ]
+        }
+      },
+    {
+        timestamps: true
+    }
+)
+
+const Race = mongoose.model('Race', raceSchema);
+
+module.exports = Race;
