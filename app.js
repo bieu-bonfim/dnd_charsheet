42f6117b86bc0a399425f60967459b94bcb3a406
Bieu <gabrielmmbonfim@gmail.com>
2023-12-06 16:32:30 -0300

    refix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-const PORT = 80 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
 app.use('/api/character', characterRoute);
