79d9df4bfe1621ba564309cdacb1a6777b0cec28
Bieu <gabrielmmbonfim@gmail.com>
2023-12-11 19:07:34 -0300

    api workabilityy

--- controllers/diceRollController.js
+++ controllers/diceRollController.js
@@ -1,8 +1,20 @@
 const asyncHandler = require('express-async-handler');
+const rolled = 0;
 
 const getRoll = asyncHandler(async(req, res) => {
   try {
-    res.send({roll: 3});
+    res.send({roll: rolled});
+  } catch (error) {
+    res.sendStatus(500);
+    throw new Error(error.message);
+  }
+});
+
+const saveRoll = asyncHandler(async(req, res) => {
+  try {
+    const value = req.body.value;
+    rolled = value;
+    res.send({roll: rolled});
   } catch (error) {
     res.sendStatus(500);
     throw new Error(error.message);
@@ -10,5 +22,6 @@ const getRoll = asyncHandler(async(req, res) => {
 });
 
 module.exports = {
-  getRoll
+  getRoll,
+  saveRoll
 }
--- routes/diceRollRoute.js
+++ routes/diceRollRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const {
-    getRoll
+    getRoll,
+    saveRoll
 } = require('../controllers/diceRollController'); 
 
 const router = express.Router();
 
 router.get('/', getRoll);
+router.post('/', saveRoll);
 
 module.exports = router;
